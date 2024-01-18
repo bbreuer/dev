45ab099280a2d0ab369a0f1f37f8aba41aa1e183
bbreuer <bbreuer445@gmail.com>
2024-01-18 16:36:52 -0500

    git push

--- darkmode.js
+++ darkmode.js
@@ -1,23 +1,25 @@
-// script.js
+// darkmode.js
 
 document.addEventListener('DOMContentLoaded', function () {
-    const darkModeToggle = document.getElementById('darkModeToggle');
+    const darkModeToggleImage = document.getElementById('darkModeToggleImage');
     const body = document.body;
 
     // Check for user's preference in local storage
     if (localStorage.getItem('darkMode') === 'enabled') {
         body.classList.add('dark-mode');
+        darkModeToggleImage.src = 'dark-bulb.png'; // Change to the dark mode image source
     }
 
-    // Toggle dark mode on button click
-    darkModeToggle.addEventListener('click', function () {
+    // Toggle dark mode on image click
+    darkModeToggleImage.addEventListener('click', function () {
         body.classList.toggle('dark-mode');
-        
 
-        // Save user's preference in local storage
+        // Toggle image source based on dark mode state
         if (body.classList.contains('dark-mode')) {
+            darkModeToggleImage.src = 'bulb_on.png'; // Change to the dark mode image source
             localStorage.setItem('darkMode', 'enabled');
         } else {
+            darkModeToggleImage.src = 'bulb_off.png'; // Change to the light mode image source
             localStorage.setItem('darkMode', 'disabled');
         }
     });
